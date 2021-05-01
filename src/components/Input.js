43b2a48e4ac8e84e86a4355d9a840922d36dbef7
michael beamer <beamermichael@gmail.com>
2021-05-01 16:24:42 +0200

    added navbar

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,20 +2,17 @@ import React from "react";
 import clsx from "clsx";
 
 export default function Input(props) {
-  const { type = "text", placeholder, ...rest } = props;
-  console.log(props);
-  console.log(props.type);
-  console.log(type);
-  //   const classes = clsx({ type: type });
-
-  function handleSearchChange() {}
+  const { type = "text", placeholder, className, ...rest } = props;
+  const classes = clsx({ input: true }, className);
 
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      value="value"
-      {...rest}
-    ></input>
+    <label className="label">
+      <input
+        type={type}
+        placeholder={placeholder}
+        className={classes}
+        {...rest}
+      ></input>
+    </label>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import Input from "./Input.js";
 
 export default function Navbar() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    console.log(searchQuery);
+  }
   return (
     <nav className="navbar">
       <img src={logo} alt="logo"></img>
-      <Input
-        type="email"
-        placeholder="Search for recipes"
-        className="red"
-        onClick={() => {}}
-      ></Input>
+      <form className="nav-form" onSubmit={handleSearchSubmit}>
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="nav-search"
+          value={searchQuery}
+          onChange={(event) => {
+            setSearchQuery(event.target.value);
+          }}
+        ></Input>
+      </form>
     </nav>
   );
 }
