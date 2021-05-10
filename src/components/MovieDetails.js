a76e39d2faac09d7f774f7069367ccc30e669df7
michael beamer <beamermichael@gmail.com>
2021-05-10 20:56:56 +0200

    fixed image loading

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -10,8 +10,6 @@ const apiKey = process.env.REACT_APP_MOVIE_KEY;
 
 export default function MovieDetails() {
   const [movie, setMovie] = useState([]);
-  // const [similarMovies, setSimilarMovies] = useState([]);
-
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -38,7 +36,6 @@ export default function MovieDetails() {
         const response = await fetch(
           `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=similar`
         );
-
         if (!response.ok) throw new Error("Problem getting movie details.");
         const data = await response.json();
         console.log(data);
@@ -49,21 +46,32 @@ export default function MovieDetails() {
     })();
   }, [id]);
 
-  const backgroundImage = {
-    backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    objectFit: "cover",
-  };
+  function backgroundImage() {
+    if (movie.backdrop_path === null || movie.backdrop_path === undefined)
+      return {
+        backgroundImage: `url(https://res.cloudinary.com/dxdboxbyb/image/upload/v1620052094/ayi6tvyiedrlmjiim6yn.png)`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        objectFit: "cover",
+      };
+
+    return {
+      backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      objectFit: "cover",
+    };
+  }
 
   return (
     <>
-      <div className="header" style={backgroundImage}>
+      <div className="header" style={backgroundImage()}>
         <div className="poster">
           <img
             src={
-              movie.poster_path === null
+              movie.poster_path === null || movie.poster_path === undefined
                 ? "https://res.cloudinary.com/dxdboxbyb/image/upload/v1620052094/ayi6tvyiedrlmjiim6yn.png"
                 : `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
             }
@@ -100,6 +108,23 @@ export default function MovieDetails() {
             <h3>Plot</h3>
             <p>{movie.overview}</p>
           </div>
+          <ul>
+            <li>
+              <span>Status</span> {movie.status}
+            </li>
+            <li>
+              <span>Budget</span>
+              {movie.budget === 0
+                ? "/"
+                : `$${new Intl.NumberFormat("en-IN").format(movie.budget)}`}
+            </li>
+            <li>
+              <span>Revenue</span>{" "}
+              {movie.revenue === 0
+                ? "/"
+                : `$${new Intl.NumberFormat("en-IN").format(movie.revenue)}`}
+            </li>
+          </ul>
         </div>
         <div className="overlay"></div>
       </div>
